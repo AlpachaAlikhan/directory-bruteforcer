63761700d4d9fe86e95ddd95b425b8e154e40c50
AlpachaAlikhan <nariman.gusseinov277nariman@gmail.com>
2026-08-01 12:40:46 +0500

    Get_target, get_wordlist are done

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ def main():
     pass
 
 def get_target():
-    pass
+    while True:
+        target = input("Enter target URL: ").strip()
+
+        if not target.startswith(("http://", "https://")):
+            print("URL must start with http:// or https://")
+            continue
+
+        return target.rstrip("/")
 
 def get_wordlist():
+    while True:
+        wordlist_name = input("Enter wordlist:")
+
+        with open(f'{wordlist_name}.txt', 'r', encoding='utf-8') as file:
+            words = "".join(file.readlines()).split("\n")
+            return words
+def load_words(wordlist):
     pass
 
-def scan(target, wordlist):
+def scan(target, words):
     pass
 
 def print_results(results):
     pass
+
+print(get_wordlist())
